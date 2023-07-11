4b0dd1fc220bfe26ee39f6e45e029016644cec34
matthieuml <merigotmatthieu@gmail.com>
2023-07-11 21:41:50 +0200

    Final bug fix

--- back/galerie/settings.py
+++ back/galerie/settings.py
@@ -19,7 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ['DEBUG'])
+if os.environ['DEBUG'] == 'False':
+    DEBUG = False
+else:
+    DEBUG = True
 
 if DEBUG:
     ALLOWED_HOSTS = [
