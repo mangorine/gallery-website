3dc6e95dfbf15a8137032228511093eda037b7eb
adcrry <ducorroyalexandre@gmail.com>
2023-07-12 17:21:49 +0200

    fix media volume

--- back/galerie/loader.py
+++ back/galerie/loader.py
@@ -5,7 +5,7 @@ from api.models import (
 )
 
 def load_folder_into_gallery(folder_dir, gal):
-    for filename in os.listdir(str(settings.BASE_DIR) + "/"+ folder_dir):
+    for filename in os.listdir(str(settings.BASE_DIR) + folder_dir):
         temp = filename.split('.')
         file = File(file_name=temp[0], file_extension=temp[1], file_full_name=filename, link=folder_dir + filename, gallery=gal)
         file.save()
