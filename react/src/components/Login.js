b6362359df9efc8376fcdc1db5e0b1084f92dfdd
adcrry <ducorroyalexandre@gmail.com>
2023-07-14 21:09:29 +0200

    beautiful login form

--- react/src/components/Login.js
+++ react/src/components/Login.js
@@ -11,14 +11,15 @@ export default function Login() {
     return (
         <>
             <Container>
-                <Row>
-                    <Col>
+                <Row className="justify-content-center">
+                    <Col lg="5" sm="12" md="8">
                         <div className="login-container">
                             <img className="login-logo" src="/static/assets/img/logo-ponthe.png"/>
                             <form className="login-form" method="post" action="/login/">
                                 <input type="hidden" name="csrfmiddlewaretoken" value={cookie} />
                                 <input type="email" className="login-input" placeholder="prenom.nom@eleves.enpc.fr" name="username"required/>
-                                <input type="password" className="login-input" placeholder="mot de passe" name="password" required/>
+                                <input type="password" className="login-input" placeholder="Mot de passe" name="password" required/>
+                                {error && <p className="login-error">Mauvais identifiants</p>}
                                 <button type="submit" className="login-button">Se connecter</button>
                             </form>
                             <a href="/accounts/login" className="login-link">Connexion SSO</a>
