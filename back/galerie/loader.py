b976a94b7f3d5204811c03b3d043a658bab9f560
matthieuml <merigotmatthieu@gmail.com>
2023-08-09 23:01:20 +0200

    Extend celery tasks for gestion

--- back/galerie/loader.py
+++ back/galerie/loader.py
@@ -3,19 +3,26 @@ import galerie.settings as settings
 from api.models import File
 from PIL import Image, ImageOps
 import zipfile
+from api.models import Gallery
 from celery import shared_task
 
 def load_zip_into_gallery(zip_dir, gal):
+    unzip(zip_dir, gal.slug)
+    generate_thumbnails.delay(gal.slug)
+    load_folder_into_gallery.delay(gal.slug)
+
+
+def unzip(zip_dir, folder_dir):
     file = zipfile.ZipFile(str(settings.BASE_DIR) + "/media/" + zip_dir)
     file.extractall(
-        path=str(settings.BASE_DIR) + "/media/" + gal.slug + "/uploads/", pwd=None
+        path=str(settings.BASE_DIR) + "/media/" + folder_dir + "/uploads/", pwd=None
     )
-    generate_thumbnails.delay(gal.slug)
-    load_folder_into_gallery(gal.slug, gal)
 
 
 # folder_dir format has to be vap-2023/ if the gallery's folder in media folder is vap-2023 /!\ DONT FORGET THE SLASH AT THE END
-def load_folder_into_gallery(folder_dir, gal):
+@shared_task
+def load_folder_into_gallery(folder_dir):
+    gal = Gallery.objects.get(slug=folder_dir)
     for filename in os.listdir(
         str(settings.BASE_DIR) + "/media/" + folder_dir + "/uploads/"
     ):
