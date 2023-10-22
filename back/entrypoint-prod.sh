1d1f091d1c02c20414a04e25229cd834c5c11a4d
matthieuml <merigotmatthieu@gmail.com>
2023-10-22 12:01:33 +0200

    Add redis container, healthchecks and docker-compose cleanup

--- back/entrypoint-prod.sh
+++ back/entrypoint-prod.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+echo "Starting celery..."
+celery -A galerie worker --loglevel=info --logfile=/var/log/celery.log --detach
+
+echo "Checking deployment..."
 python3 manage.py check --deploy
 
 echo "Copying static files..."
 python3 manage.py collectstatic --noinput
 
-echo "Waiting for postgres..."
+echo "Check if PostgreSQL started..."
 while ! nc -z $DB_HOST $DB_PORT; do
     sleep 0.1
 done
@@ -13,12 +17,6 @@ echo "PostgreSQL started."
 
 echo "Migrating..."
 python3 manage.py migrate --noinput
-
-echo "Starting redis server..."
-redis-server 1>/dev/null 2>&1 &
-
-echo "Starting celery..."
-celery -A galerie worker --loglevel=info --logfile=/var/log/celery.log --detach
 
 echo "Starting gunicorn server..."
 
